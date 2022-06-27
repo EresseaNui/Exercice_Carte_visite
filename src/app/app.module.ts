e5efd5e261da701c2fcb88c6c2d7926daa77b0d0
EresseaNui <leteneur.roland@hotmail.fr>
2022-06-27 16:59:15 +0200

    add visit Card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BusinessCardComponent } from './business-card/business-card.component';
+import { TitleComponent } from './title/title.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, BusinessCardComponent, TitleComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
